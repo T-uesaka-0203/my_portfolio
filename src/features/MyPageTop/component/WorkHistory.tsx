0fdcd5a7f9d2e4473e7c21cbbe0f2414dc17fd7e
T-uesaka-0203 <mou217949@gmail.com>
2024-05-10 05:24:26 +0900

    カルーセルを画面幅が狭い場合に縦のカード&矢印をPC以外では消す

--- src/features/MyPageTop/component/WorkHistory.tsx
+++ src/features/MyPageTop/component/WorkHistory.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTheme, useMediaQuery } from '@mui/material';
 import { Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -11,6 +12,7 @@ import my_image from 'assets/images/my_image.jpeg';
 import uzone_image from 'assets/images/Uzone_Image.png'
 import WorkListIcon from '@mui/icons-material/Checklist';
 import { styled } from '@mui/system';
+import { isIOS, isAndroid } from 'react-device-detect';
 
 // カードのスタイルを定義
 const StyledCard = styled(Card)({
@@ -38,19 +40,19 @@ interface SlideItemProps {
   additionalComments: string[];
 }
 
-const EachSlide: React.FC<SlideItemProps> = ({ title, imagePath, href, hrefDetail, additionalComments }) => {
+const EachSlideWide: React.FC<SlideItemProps> = ({ title, imagePath, additionalComments }) => {
   return (
-    <Grid item xs={12} style={{ display: 'flex', marginLeft:'5%', marginRight:'5%' }}>
+    <Grid item xs={12} sx={{ display: 'flex', marginLeft:'5%', marginRight:'5%' }}>
       <StyledCard sx={{ display: 'flex', marginLeft:'10%', marginRight:'10%', borderRadius: 5 }} onClick={openUzone}>
         <CardContent sx={{ flex: 1, padding: 0 , width: "50%", display: "block", flexDirection: "column", justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} >
           <CardMedia component="img" sx={{ objectFit: 'cover' }} image={imagePath} />
         </CardContent>
-        <CardContent sx={{ flex: 1, width: "50%", display: "block", flexDirection: "column", justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} >
-          <Typography variant="body1" gutterBottom style={{ fontWeight: 'bold', fontSize: 'clamp(20px, 3vw, 30px)', marginBottom:'15%' }} >
+        <CardContent sx={{ flex: 1, width: "50%", padding:0, display: "block", flexDirection: "column", justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} >
+          <Typography variant="body1" gutterBottom sx={{ fontWeight: 'bold', fontSize: 'clamp(20px, 3vw, 30px)', marginBottom:{ xs: '10%', sm: '10%', md: '12%', lg:"15%" }, marginTop:{ xs: '10px', sm: '10px', md: '10px', lg:"16px" } }} >
            {title}
           </Typography>
           {additionalComments.map((comment) => (
-              <Typography variant="body1" gutterBottom style={{ fontWeight: 'bold', fontSize: 'clamp(12px, 3vw, 20px)' }} >
+              <Typography variant="body1" gutterBottom sx={{ fontWeight: 'bold', fontSize: { xs: '10px', sm: '12px', md: '15px', lg:"20px" } }} >
                 {comment}
               </Typography>
             ))}
@@ -60,8 +62,31 @@ const EachSlide: React.FC<SlideItemProps> = ({ title, imagePath, href, hrefDetai
   );
 };
 
+const EachSlideNarrow: React.FC<SlideItemProps> = ({ title, imagePath, additionalComments }) => {
+  return (
+    <Grid item xs={12} sx={{ height:"90vw", marginLeft:'10%', marginRight:'10%' }}>
+      <StyledCard sx={{ width:"100%", height:"100%" ,borderRadius: 8 }} onClick={openUzone}>
+        <CardMedia component="img" height="50%" image={imagePath} />
+        <CardContent sx={{ height: "40%", display: "block", flexDirection: "column", textAlign: 'center' }} >
+          <Typography variant="body1" gutterBottom sx={{ fontWeight: 'bold', fontSize: 'clamp(20px, 5vw, 40px)', marginBottom:'clamp(10%, 3vw, 10%)' }} >
+           {title}
+          </Typography>
+            {additionalComments.map((comment) => (
+                <Typography variant="body1" sx={{ fontWeight: 'bold', fontSize: 'clamp(12px, 3vw, 30px)', marginTop:"2px" }} >
+                  {comment}
+                </Typography>
+              ))}
+        </CardContent>
+      </StyledCard>
+    </Grid>
+  );
+};
 
 const WorkHistory: React.FC = () => {
+  
+  const displayStyle = isAndroid || isIOS ? "none" : "block";
+  const isSmallScreen = useMediaQuery('(max-width: 1000px)');
+
   return(
     <div>
       <div id="works" className="works" style={{ display: 'block', justifyContent: 'center'}}>
@@ -74,6 +99,7 @@ const WorkHistory: React.FC = () => {
       <Grid sx={{ width:'100%', justifyContent:'center'}}>
         <Carousel
 
+          sx={{ height:"100%" }}
           NextIcon={<ArrowForwardIosSharpIcon/>} //矢印アイコンを別のアイコンに変更
           PrevIcon={<ArrowBackIosSharpIcon/>} //矢印アイコンを別のアイコンに変更
           autoPlay = {false} //自動でCarouselを動かすかどうか(true or false)
@@ -84,13 +110,13 @@ const WorkHistory: React.FC = () => {
 
           indicatorContainerProps={{
             style: {
-              margin: "3px 0px 0px 0px",
+              marginTop: "clamp(20px, 5vh, 40px)",
               position: "relative"
             }
           }}
           indicatorIconButtonProps={{//アクティブでない下の丸いアイコンの設定
             style: {
-                marginTop: '100px',
+                marginTop: "0px",
                 padding: '10px',//位置調整
                 color: Icon,
             }
@@ -102,9 +128,10 @@ const WorkHistory: React.FC = () => {
           }}
           navButtonsWrapperProps={{//矢印ボタン周りの設定
             style: {
-              margin: '-20px 10% 0px', //位置調整
+              display: displayStyle,
+              margin: '0 2%', //位置調整
               alignItems:'center',
-              // height:'100%'
+              height: "80%" ,
               position:'absolute'
             },
           }}
@@ -115,9 +142,18 @@ const WorkHistory: React.FC = () => {
                 borderRadius : 100,//0にすると四角になる．
            },
           }}>
-          <EachSlide title="Uzone" imagePath={uzone_image} href='https://uzone.parts/' hrefDetail="Uzoneはこちら" additionalComments={["自動車の中古部品に特化したECサイト", "API開発を担当", "データベース周りの調整を担当", "部品適合ロジックの構築を担当"]}/>
-          <EachSlide title="Miztory" imagePath={my_image} href='' hrefDetail="外部非公開です" additionalComments={["外部システムとバッチで連携", "フルスタックで開発", "研究室内でサーバーを立てて稼働"]}/>
-          <EachSlide title="社内DXツール" imagePath={my_image} href='' hrefDetail="外部非公開です" additionalComments={["GASで実行APIの実装", "Google系のAPI（VisionやGmail）との連携", "GUI、Window、ブラウザ操作の実装"]}/>
+          {isSmallScreen 
+            ? <EachSlideNarrow title="Uzone" imagePath={uzone_image} href='https://uzone.parts/' hrefDetail="Uzoneはこちら" additionalComments={["自動車の中古部品に特化したECサイト", "API開発を担当", "データベース周りの調整を担当", "部品適合ロジックの構築を担当"]}/>
+            : <EachSlideWide title="Uzone" imagePath={uzone_image} href='https://uzone.parts/' hrefDetail="Uzoneはこちら" additionalComments={["自動車の中古部品に特化したECサイト", "API開発を担当", "データベース周りの調整を担当", "部品適合ロジックの構築を担当"]}/>
+          }
+          {isSmallScreen 
+            ? <EachSlideNarrow title="Miztory" imagePath={my_image} href='' hrefDetail="外部非公開です" additionalComments={["研究室内のデータを統合・表示するシステム", "外部システムとバッチで連携", "フルスタックで開発", "研究室内でサーバーを立てて稼働"]}/>
+            : <EachSlideWide title="Miztory" imagePath={my_image} href='' hrefDetail="外部非公開です" additionalComments={["研究室内のデータを統合・表示するシステム", "外部システムとバッチで連携", "フルスタックで開発", "研究室内でサーバーを立てて稼働"]}/>
+          }
+          {isSmallScreen 
+            ? <EachSlideNarrow title="社内DXツール" imagePath={my_image} href='' hrefDetail="外部非公開です" additionalComments={["GASで実行APIの実装", "Google系のAPI（VisionやGmail）との連携", "GUI、Window、ブラウザ操作の実装"]}/>
+            : <EachSlideWide title="社内DXツール" imagePath={my_image} href='' hrefDetail="外部非公開です" additionalComments={["GASで実行APIの実装", "Google系のAPI（VisionやGmail）との連携", "GUI、Window、ブラウザ操作の実装"]}/>
+          }
        </Carousel>
       </Grid>
     </div>
